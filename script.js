adcae2b6b73050560ef486aa1c1a396acf14fdcd
daviddserb <83017887+daviddserb@users.noreply.github.com>
2021-08-25 09:11:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-var width = 9, height = width;
-var nrBombs = 10;
+const width = 9, height = width;
+const grid = document.getElementById("grid");
+const nrBombs = 10;
 var squares = [];
-const grid = document.querySelector('.grid'); //salvez tabla de joc din HTML si CSS (cred?)
 
 function generateBoard() {
   //construim tabelul de joc
-  const normalsArray = Array(width * height - nrBombs).fill('notBombs');
-  const bombsArray = Array(nrBombs).fill('bombs');
+  const normalsArray = Array(width * height - nrBombs).fill("notBombs");
+  const bombsArray = Array(nrBombs).fill("bombs");
+  const concatArray = normalsArray.concat(bombsArray)
+  const shuffledArray = concatArray.sort(() => Math.random() - 0.5);
+  alert(shuffledArray) //MERGE!!!
 
   for (let i = 0; i < width*height; ++i) {
-    var square = document.createElement('div'); //creez 81 de div-uri (adica patratelele)
-    square.setAttribute('id', i); //atribui fiecarui patratel, un id cu nr. unic
+    var square = document.createElement("div"); //creez 81 de div-uri (adica patratelele)
+    square.setAttribute("id", i); //atribui fiecarui patratel, un id cu nr. unic
+    //Acum trebuie in square (adica div, patratelul) sa salvez valorile din shuffledArray.
+    //Am incercat mai multe chestii si nu merg: square = shuffledArray[i], add(), push(), etc...
     grid.appendChild(square); //punem patratelele in tabla de joc
+    squares.push(square);
   }
 
   //vedem unde avem bombe si in functie de asta, punem nr. in patrate
   for(let i = 0; i < width*height; ++i) {
+    let nrInSquare = 0;
 
   }
-
 }
 
 generateBoard();
